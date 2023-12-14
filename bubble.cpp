6313be88a79e83ef8999c07114832c441f893f0a
Avinash <53809765+avinash7375@users.noreply.github.com>
2023-12-14 14:22:44 +0530

    Update bubble.cpp

--- bubble.cpp
+++ bubble.cpp
@@ -5,21 +5,26 @@ using namespace std;
 // An optimized version of Bubble Sort
 void bubbleSort(int arr[], int n)
 {
-	int i, j;
-	bool swapped;
+	int i, j, k;
+// 	bool swapped;
 	for (i = 0; i < n - 1; i++) {
-		swapped = false;
+// 		swapped = false;
+//      printing every array of sorted elements
+	    for (k = 0; k < n; k++){
+		    cout << " " << arr[k];
+	}
+	cout << endl;
 		for (j = 0; j < n - i - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
 				swap(arr[j], arr[j + 1]);
-				swapped = true;
+				// swapped = true;
 			}
 		}
 
 		// If no two elements were swapped
 		// by inner loop, then break
-		if (swapped == false)
-			break;
+// 		if (swapped == false)
+// 			break;
 	}
 }
 
